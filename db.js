6495d9e88e8eac98779ef047238c66ea0215b078
Nelson Ramirez <xhyskoax@gmail.com>
2023-03-06 19:46:48 -0300

    primera consulta la base de datos usando pg desde node

---
+++ db.js
@@ -0,0 +1,24 @@
+const {Client} = require('pg'); 
+
+const client = new Client({
+    host: 'localhost',
+    user: 'node',
+    password: '123456',
+    port: 5432,
+    database: 'm7_usuarios'
+})
+
+/* client.connect(error => {
+    if(error){
+        console.log(error.message)
+        console.error("Ha ocurrido un problema al conectarse a la base de datos.")
+    }else{
+        console.log("Se ha conectado a la base de datos m7_usuarios.")
+    }
+}) */
+client.connect()
+
+client.query("SELECT * FROM usuarios", (error, result) => {
+    if(error) console.error(error)
+    console.table(result.rows);
+})
